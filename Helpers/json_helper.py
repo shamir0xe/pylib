27dde81c22591fef54fd96bd0919e50dd1134bc7
shamir0xe <shamir0xe@gmail.com>
2021-02-07 05:17:31 +0330

    modified JsonHelper class, moved json_parser to json_helper

--- Helpers/json_helper.py
+++ Helpers/json_helper.py
@@ -13,7 +13,7 @@ class JsonHelper:
             if key in js_1:
                 if isinstance(value, list):
                     if isinstance(js_1[key], list):
-                        res[key] = js_1[key] + value
+                        res[key] = js_1[key] + value 
                     else:
                         res[key] = value
                 elif isinstance(value, dict):
@@ -26,15 +26,6 @@ class JsonHelper:
             if not key in res:
                 res[key] = value
         return res
-    
-    @staticmethod
-    def __selector_array_index(token):
-        if token == '*':
-            return -2
-        z = re.match(r'(__)(\d+)', token)
-        if z:
-            return int(z.groups()[1])
-        return -1
 
     @staticmethod
     def selector_get_value(js, selector):
@@ -98,8 +89,77 @@ class JsonHelper:
                     # TODO
                     pass
         return res
+
+    @staticmethod
+    def __selector_array_index(token):
+        if token == '*':
+            return -2
+        z = re.match(r'(__)(\d+)', token)
+        if z:
+            return int(z.groups()[1])
+        return -1
             
+class JsonParser:
+    def __init__(self, json_file):
+        self.json = json_file
+    
+    def parse(self, selectors):
+        rev_map = {}
+        for key, value, in selectors.items():
+            rev_map['.' + value] = key
+        self.__selectors = rev_map
+        self.__out = {}
+        self.__traverse('', self.json)
+        return self.__out
 
+    def __currected_path(self, path):
+        if len(path) < 2:
+            return ''
+        path = re.sub(r'(\.__\d+)(\.|$)+', '.*.', path)
+        path = path[:-1] if path[-1] == '.' else path
+        return path
 
+    def __check_path(self, path):
+        return self.__currected_path(path) in self.__selectors
 
-           
+    def __traverse(self, path, obj):
+        if self.__check_path(path):
+            self.__add_to_output(path, obj)
+        if isinstance(obj, list):
+            cur = 0
+            for next_obj in obj:
+                self.__traverse(path + '.__{}'.format(cur), next_obj)
+                cur += 1
+        elif isinstance(obj, dict):
+            for attr, next_obj in obj.items():
+                self.__traverse(path + '.' + attr, next_obj)
+    
+    @staticmethod
+    def __is_array(token):
+        z = re.match(r'(__)(\d+)', token)
+        if z:
+            return int(z.groups()[1])
+        return -1
+
+    def __add_to_output(self, path, obj):
+        tokens = path[1:].split('.')
+        walker = self.__out
+        for i in range(len(tokens)):
+            token = tokens[i]
+            index = self.__is_array(token)
+            if index >= 0:
+                while len(walker[tokens[i - 1]]) <= index:
+                    walker[tokens[i - 1]].append({})
+                if i + 1 >= len(tokens):
+                    walker[tokens[i - 1]][index] = obj
+                walker = walker[tokens[i - 1]][index]
+            else:
+                if i + 1 < len(tokens) and self.__is_array(tokens[i + 1]) >= 0:
+                    if not token in walker:
+                        walker[token] = []
+                else:
+                    if not token in walker:
+                        walker[token] = {}
+                    if i + 1 >= len(tokens):
+                        walker[token] = obj
+                    walker = walker[token]
