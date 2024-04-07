97e96f8cd7219f5ec5ee7c1a068650507d9f8895
shamir0xe <shamir0xe@gmail.com>
2024-04-08 01:09:10 +0330

    refactor: add docstring and modified

--- data/data_transfer_object.py
+++ data/data_transfer_object.py
@@ -1,39 +1,52 @@
 from __future__ import annotations
 from dataclasses import asdict, dataclass, fields
+from typing import Any, Dict
 
 
 @dataclass
 class DataTransferObject:
-    def __init__(self, **kwargs) -> None:
+    """
+    A generic data transfer object.
+    """
+
+    def __init__(self, **kwargs: Any) -> None:
         self.__dict__ = kwargs
 
     @classmethod
-    def from_dict(
-        cls,
-        obj: dict,
-    ):
-        res: dict = {}
-        field_names = [field.name for field in fields(cls)]
+    def from_dict(cls, obj: Dict[str, Any]) -> DataTransferObject:
+        """
+        Constructs an instance of the class from a dictionary.
+        """
+        dto_kwargs = {}
+        field_names = {field.name for field in fields(cls)}
         for key, value in obj.items():
             if key in field_names:
-                res[key] = value
-        dto = cls(**res)
+                dto_kwargs[key] = value
+        dto = cls(**dto_kwargs)
         for key, value in dto.__dict__.items():
-            if hasattr(dto, f"{key}_mapper"):
-                setattr(dto, key, getattr(dto, f"{key}_mapper")(value))
+            mapper_func = getattr(dto, f"{key}_mapper", None)
+            if mapper_func:
+                setattr(dto, key, mapper_func(value))
         return dto
 
     def __str__(self) -> str:
-        res = ""
-        res += "*" * 11 + "\n"
-        for key, value in self.__dict__.items():
-            res += f"[{key}] = {value}\n"
-        res += "*" * 11 + "\n"
-        return res
+        """
+        Returns a string representation of the object.
+        """
+        attributes = "\n".join(
+            [f"[{key}] = {value}" for key, value in self.__dict__.items()]
+        )
+        return f"************\n{attributes}\n************"
 
-    def add_attributes(self, **kwargs) -> None:
+    def add_attributes(self, **kwargs: Any) -> None:
+        """
+        Dynamically adds attributes to the object.
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def to_dict(self) -> dict:
+    def to_dict(self) -> Dict[str, Any]:
+        """
+        Converts the object to a dictionary.
+        """
         return asdict(self)
