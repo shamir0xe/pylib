c89476e8f2f90a258fe3d7d99758a43ceb6a6769
shamir0xe <shamir0xe@gmail.com>
2024-04-09 01:08:34 +0330

    refactor: change the behavior of the config/config.py

--- config/config.py
+++ config/config.py
@@ -1,33 +1,33 @@
 from typing import Any
+from dataclasses import dataclass
 from ..file.file import File
 from ..path.path_helper import PathHelper
 from ..json.json_helper import JsonHelper
 
 
+@dataclass
 class Config:
-    DEFAULT_CONFIG_FOLDER_NAME = "configs"
+    file_path: str = __file__
+    file_format: str = ".json"
+    folder_name: str = "configs"
 
-    def __init__(
-        self,
-        filename: str,
-        config_folder_name: str = DEFAULT_CONFIG_FOLDER_NAME,
-    ) -> None:
-        filename += ".json"
-        self.json = File.read_json(
-            PathHelper.from_root(__file__, config_folder_name, filename)
+    def get(self, filename: str, selector: str = "", default: Any = None) -> Any:
+        json = File.read_json(
+            PathHelper.from_root(
+                self.file_path, self.folder_name, filename + self.file_format
+            )
         )
-
-    def get(self, selector: str = "", default: Any = None) -> Any:
-        value = JsonHelper.selector_get_value(self.json, selector)
+        value = JsonHelper.selector_get_value(json, selector)
         if value != {}:
             return value
         return default
 
-    @staticmethod
-    def read(selector: str, **kwargs) -> Any:
+    def read(self, selector: str, **kwargs) -> Any:
         index = selector.find(".")
         if index < 0:
-            return Config(selector).get(**kwargs)
-        filename = selector[:index]
-        selector = selector[index + 1 :]
-        return Config(filename).get(selector, **kwargs)
+            filename = selector
+            selector = ""
+        else:
+            filename = selector[:index]
+            selector = selector[index + 1 :]
+        return self.get(filename, selector, **kwargs)
